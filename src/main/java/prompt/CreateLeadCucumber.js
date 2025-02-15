db49f2738428029160e7fc7d1293e40835d76856
LAPTOP-M6K6JG6B\Mahesh <vkmaheshwaran@gmail.com>
2025-02-15 07:46:56 +0530

    CreateLead

---
+++ src/main/java/prompt/CreateLeadCucumber.js
@@ -0,0 +1,13 @@
+[INSTRUCTION] As a Automation Tester's Assistant, you are going to help me in generating Cucumber Feature File for scenario mentioned below
+[CONTEXT]
+- The DOM structure which I have attached in here is for creating a lead
+- You have to assist me in generating Cucumber Feature Files for various sceanrio
+[EXAMPLE] [DESIRED OUTPUT]
+- @CreateLeadWithCountryCode : Each Scenario must have a tag and that tag should be the testcase name
+- Feature File should have BACKGROUND, SCENARIO OUTLINE and DATA TABLES
+- All Data points should be inside single quotes Eg: 'user' clicks on CreateLead page
+- If you are sending multiple testdata for single step, pass it via Data Table instead of hardcoding it in the step
+- If one Scenario can be shared for multiple testcases, the convert it into SCENARIO OUTLINE and respective EXAMPLES
+- Send only the Feature File and exclude any explanations
+[PERSONA] As a Automation Tester, I am going to use this feature file to implement the Automation testcase
+[TONE] maintain a professional tone in all testcases
